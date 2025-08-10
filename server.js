9fa4e5405d2b8f90bc73bb7c9dae1c9d32681dfd
KimLuanDev <Troll.Jayd@gmail.com>
2025-08-10 13:18:09 +0700

    Thêm Game

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Cho phép Express phục vụ file tĩnh từ thư mục "public"
-app.use(express.static('public'));
+app.use(express.static('Lucky77'));
 
 app.listen(PORT, () => {
   console.log(`✅ Server đang chạy tại: http://localhost:${PORT}`);
